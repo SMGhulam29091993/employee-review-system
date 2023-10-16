837610fdf2bf1ce97306b1546bf6786a5958f80a
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2023-10-17 02:07:03 +0530

    installing the ejs and setting up the ejs view engine

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const app = express();
 
 const port = 8000;
 
+// connecting with the db
 const db = require('./config/mongoose');
 
+// to read the req from users
+app.use(express.urlencoded());
+
+
+// setting ejs view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, (err)=>{
     if(err){
