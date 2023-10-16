4aef890002f42057d216696d8072f79013129553
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2023-10-17 02:17:14 +0530

    installed express-ejs-layouts and to create partial views

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ const app = express();
 
 const port = 8000;
 
+const expressLayout = require('express-ejs-layouts');
+
+
+
 // connecting with the db
 const db = require('./config/mongoose');
 
 // to read the req from users
 app.use(express.urlencoded());
 
+app.use(expressLayout);
+// Middleware to serve static files
+app.use(express.static('assets'));
+
+// to extract the style and scripts from partial views
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 // setting ejs view engine
 app.set('view engine', 'ejs');
